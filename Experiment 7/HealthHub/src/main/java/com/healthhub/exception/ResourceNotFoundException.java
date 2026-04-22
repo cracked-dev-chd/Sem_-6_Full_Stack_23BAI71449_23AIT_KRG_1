ccbc88f8654b21731fa8a0de5709570dd38846d2
cracked-dev-chd <23BAI71449@cuchd.in>
2026-04-22 10:36:46 +0530

    Delete Experiment 7/HealthHub directory

--- Experiment 7/HealthHub/src/main/java/com/healthhub/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.healthhub.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- Experiment 7/HealthHub/src/main/java/com/healthhub/repository/PatientRepo.java
+++
@@ -1,8 +0,0 @@
-package com.healthhub.repository;
-
-import com.healthhub.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepo extends JpaRepository<Patient, Long> {
-
-}
